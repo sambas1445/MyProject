6aa45ffe68b9017c8fb7662dd50d65ffae139057
root <root@ip-172-31-11-80.ca-central-1.compute.internal>
2019-12-29 04:16:03 +0000

    ci/cd added

--- myproj/src/main/java/com/shiva/App.java
+++ myproj/src/main/java/com/shiva/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World welcome to jenkins again!" );
+        System.out.println( "Hello World welcome to jenkins with ci/cd pipeline" );
     }
 }
