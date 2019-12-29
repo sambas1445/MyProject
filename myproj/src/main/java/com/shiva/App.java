234fe025615911c9d715b8ff603a2dafbb7ba38e
sambas1445 <58957131+sambas1445@users.noreply.github.com>
2019-12-28 23:21:17 -0500

    Update App.java

--- myproj/src/main/java/com/shiva/App.java
+++ myproj/src/main/java/com/shiva/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World welcome to jenkins with ci/cd pipeline" );
+        System.out.println( "Hello World welcome to jenkins with ci/cd pipeline1" );
     }
 }
